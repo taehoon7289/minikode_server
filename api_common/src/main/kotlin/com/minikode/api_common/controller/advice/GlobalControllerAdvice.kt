4003d03792d7a30a96152ab1d8cb2865f03585c0
taehoon7289 <taehoon7289@gmail.com>
2023-01-05 12:43:27 +0900

    feat: GlobalControllerAdvice.kt 수정

--- api_common/src/main/kotlin/com/minikode/api_common/controller/advice/GlobalControllerAdvice.kt
+++ api_common/src/main/kotlin/com/minikode/api_common/controller/advice/GlobalControllerAdvice.kt
@@ -1,7 +1,10 @@
 package com.minikode.api_common.controller.advice
 
+import com.minikode.api_common.vo.ResponseVo
 import com.minikode.common.exception.ServiceException
 import com.minikode.common.exception.ServiceRuntimeException
+import com.minikode.common.exception.code.CommonExCode
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -9,13 +12,19 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class GlobalControllerAdvice {
 
     @ExceptionHandler(Exception::class)
-    fun exception(): String {
-        return "exception"
+    fun exception(ex: Exception): ResponseEntity<ResponseVo> {
+        return ResponseEntity.ok(ResponseVo(
+            code = CommonExCode.CALL_MANAGER.code,
+            message = CommonExCode.CALL_MANAGER.msg,
+        ))
     }
 
     @ExceptionHandler(ServiceRuntimeException::class)
-    fun serviceRuntimeException(): String {
-        return "serviceRuntimeException"
+    fun serviceRuntimeException(ex: ServiceRuntimeException): ResponseEntity<ResponseVo> {
+        return ResponseEntity.ok(ResponseVo(
+            code = ex.exCode.code,
+            message = ex.message ?: ex.exCode.msg,
+        ))
     }
 
     @ExceptionHandler(ServiceException::class)
--- common/src/main/kotlin/com/minikode/common/exception/code/CommonExCode.kt
+++ common/src/main/kotlin/com/minikode/common/exception/code/CommonExCode.kt
@@ -5,6 +5,7 @@ enum class CommonExCode(
     override val msg: String?
 ) : BaseExCode {
 
+    CALL_MANAGER(-9999, "관리자에게 문의하세요."),
     NO_EXIST_DATA(-10000, "데이터가 존재하지 않습니다."),
     INVALID_TOKEN(-10001, "유효하지 않은 토큰입니다."),
 }

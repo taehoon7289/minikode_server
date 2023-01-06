e8e6aab66e334eb34dd0bc60a90c27b1b4fc3180
taehoon7289 <taehoon7289@gmail.com>
2023-01-06 18:38:32 +0900

    feat: GlobalControllerAdvice.kt 수정

--- common/src/main/kotlin/com/minikode/common/exception/ServiceRuntimeException.kt
+++ common/src/main/kotlin/com/minikode/common/exception/ServiceRuntimeException.kt
@@ -3,8 +3,9 @@ package com.minikode.common.exception
 import com.minikode.common.exception.code.BaseExCode
 
 class ServiceRuntimeException(
-    exCode: BaseExCode,
+    val exCode: BaseExCode,
     message: String? = null,
 ) : RuntimeException() {
+
     override val message: String? = message ?: exCode.msg
 }
